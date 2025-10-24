258b089b67b153744578204bb68c28b37da14377
Hatix Ntsoa <hatixntsoa@proton.me>
2025-10-24 08:45:17 +0300

    feat: updated configuration file path to match the domain

--- cmd/bypass.cmd
+++ cmd/bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/hatixntsoa/local.windows/refs/heads/master/config/hatixntsoa.winux.xml
+curl -L -o C:\Windows\Panther\unattend.xml bypass.hatixntsoa.site/config/hatixntsoa.winux.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
